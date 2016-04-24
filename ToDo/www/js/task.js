e84108a83e25b110e71dec07a1de27d655126f45
Damian <sgt.curren@gmail.com>
2016-04-24 15:57:15 +0100

    working delete

--- ToDo/www/js/task.js
+++ ToDo/www/js/task.js
@@ -1,6 +1,6 @@
 angular.module('task', ['ionic','database'])
 
-.controller('taskCtrl', function($scope, SQLService) {
+.controller('taskCtrl', function($scope, $ionicPopup, SQLService) {
   
   //calls function which creates database
 	SQLService.setup();
@@ -19,5 +19,20 @@ angular.module('task', ['ionic','database'])
 	$scope.loadTask();
 	//sets input back to empty,null
     task.title = "";
+  };
+  
+  $scope.deleteItem = function(taskid) {
+	  //popup which confirms deletion or not
+	$ionicPopup.confirm({
+	  title: 'Confirm Delete',
+	  content: 'Deleting is permanent, are you sure?'
+	}).then(function(res) {
+	  if(res) {
+		  //if "ok", then delete task with task id from from database
+		  //then reload database info to update list
+		SQLService.del(taskid);
+		$scope.loadTask();
+	  } 
+	});
   };
 })
