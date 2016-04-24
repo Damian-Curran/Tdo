43533e7577ec394afe50edcc2ee6dd15a6ce2f09
Damian <sgt.curren@gmail.com>
2016-04-24 16:17:38 +0100

    slide in menu

--- ToDo/www/js/task.js
+++ ToDo/www/js/task.js
@@ -1,10 +1,8 @@
 angular.module('task', ['ionic','database'])
 
-.controller('taskCtrl', function($scope, $ionicPopup, SQLService) {
+.controller('taskCtrl', function($scope, $ionicModal, $ionicPopup, SQLService) {
   
-  //calls function which creates database
 	SQLService.setup();
-		//loads tasks from database
 	$scope.loadTask = function() {
 		SQLService.all().then(function (results) {
 			$scope.tasks = results;
@@ -14,31 +12,41 @@ angular.module('task', ['ionic','database'])
 	$scope.loadTask(); 
 	$scope.listCanSwipe = true;
   
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'animated ' + 'slide-in-up'
+  });
+ 
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+  
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+  
   $scope.createTask = function(task) {
-	 //adds task to database then reloads database info
 	SQLService.set(task.title);
 	$scope.loadTask();
-	//sets input back to empty,null
+	$scope.taskModal.hide();
     task.title = "";
   };
   
   $scope.deleteItem = function(taskid) {
-	  //popup which confirms deletion or not
 	$ionicPopup.confirm({
 	  title: 'Confirm Delete',
 	  content: 'Deleting is permanent, are you sure?'
 	}).then(function(res) {
 	  if(res) {
-		  //if "ok", then delete task with task id from from database
-		  //then reload database info to update list
 		SQLService.del(taskid);
 		$scope.loadTask();
 	  } 
 	});
   };
   
-  $scope.showDets = function(tasktitle) {
-	   //popup.alert used to show alternative inerface for task info, rather than changing .html
+   $scope.showDets = function(tasktitle) {
 	$ionicPopup.alert({
 	  title: 'Task Info',
 	  content: tasktitle
@@ -49,14 +57,10 @@ angular.module('task', ['ionic','database'])
 	 $ionicPopup.prompt({
 	  title: 'Enter Updates to Task'
 	}).then(function(res) {
-		//if checks to see if they clicked ok option without entering characters, which would make it undefined or null
 		if(res != "")
 		{
-			//deletes taskid saved task_name 
 			SQLService.del(taskid);
-			//sets new updated task_name
 			SQLService.set(res);
-			//loads database to refresh html page
 			$scope.loadTask();
 		}
 	});
